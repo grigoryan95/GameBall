d7217910d1d78a168a3142652ddf2a25acb318b6
grigoryan95 <grigoryanandranik95@gmail.com>
2021-08-20 16:53:55 +0400

    first push

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { SquareComponent } from './square/square.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SquareComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/square/square.component.ts
@@ -0,0 +1,44 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.scss']
+})
+export class SquareComponent {
+  level: number = 1;
+  click: number = 0;
+  isDisable: boolean = true;
+  top:number = 50;
+  left:number = 50;
+  time:number = 1000;
+  height:number = 200;
+  width:number = 200;
+
+  inContainer(): void {
+    this.click++;
+  }
+
+  ballClick(): void {
+    this.level++;
+    this.time *= 0.75;
+    this.width -= 5;
+    this.height -= 5;
+  }
+
+  reset(): void {
+    this.level = 1;
+    this.click = 0;
+    this.isDisable = true;
+  }
+
+  start(): void {
+    this.isDisable = false;
+    setTimeout(() => {
+      this.top = Math.random() * (window.innerHeight - 200);
+      this.left = Math.random() * (window.innerHeight * 0.8 - 200);
+      this.start()
+    },this.time)
+  }
+
+}
